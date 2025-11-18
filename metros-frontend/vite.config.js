ba3974499e9491b323a7d5f3e87f8991e022f6e9
ruslan051 <galatipov@yandex.ru>
2025-11-18 16:23:04 +0300

    f

--- metros-frontend/vite.config.js
+++ metros-frontend/vite.config.js
@@ -3,19 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
   build: {
     outDir: 'dist',
     sourcemap: true
   },
-  // Добавьте эти настройки:
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  },
-  // Убедитесь, что base путь правильный для вашего окружения
-  base: './'
+  server: {
+    host: true,
+    port: 3000
+  }
 })
